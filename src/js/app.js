7400b54e71f9a811f327a3512ab4f3529af34df3
iga-yamaguchi <iganin2@gmail.com>
2017-12-05 02:47:14 +0900

    Modify animation and disable scrollbar

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import anime from 'animejs';
 
 const elements = document.querySelectorAll('ul.main-link-list a');
 const animated = [];
+const easing = [.91, -0.67, .33, 1.5];
 
 elements.forEach((element, index) => {
     animated[index] = false;
@@ -15,6 +16,12 @@ elements.forEach((element, index) => {
 
         const target = event.target;
         const width = target.getBoundingClientRect().x;
+        const color = target.getAttribute('data-color') || '#5f5f5f';
+        const colorProperty = {
+            value: color,
+            // value: '#5f5f5f',
+            delay: 1700,
+        };
 
         anime({
             targets: target,
@@ -22,24 +29,39 @@ elements.forEach((element, index) => {
                 {
                     value: window.innerWidth - width - target.offsetWidth - 5,
                     duration: 500,
+                    easing,
                 },
             ],
-            easing: [.91, -0.67, .33, 1.5],
             rotate: {
                 value: '0.75turn',
-                delay: 700
+                delay: 700,
+                easing,
             },
             scale: {
                 value: 20,
                 delay: 1700
             },
-            backgroundColor: {
-                value: '#5f5f5f',
-                delay: 1300,
+            direction: 'alternative',
+            color: colorProperty,
+            // backgroundColor: colorProperty,
+            begin() {
+                document.getElementById('over')
+                    .style
+                    .display = 'block';
+                anime({
+                    targets: '#over',
+                    // display: 'block'
+                    backgroundColor: color,
+                    delay: 1800,
+                });
+                // event.target.click();
             },
-            complete() {
-                event.target.click();
-            },
+            complete(animeObj) {
+                console.log(animeObj);
+                console.log(animeObj.animatables[0].target);
+                const element = animeObj.animatables[0].target;
+                element.style.display = 'none';
+            }
         });
     };
 });
